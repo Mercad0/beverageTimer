42c708f257c496be75526d81a050f5291b8f6188
Abimael M <abimaelmercado05@gmail.com>
2023-03-21 14:30:17 -0400

    Initial Commit

---
+++ beverageTimer/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  beverageTimer
+//
+//  Created by AM on 3/21/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
