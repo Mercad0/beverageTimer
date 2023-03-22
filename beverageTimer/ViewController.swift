e6406b2268f147c10c66291cf28ceb752bc16908
Abimael M <abimaelmercado05@gmail.com>
2023-03-22 01:12:19 -0400

    Timer visible & choice of drink

--- beverageTimer/ViewController.swift
+++ beverageTimer/ViewController.swift
@@ -6,35 +6,56 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
     
-    let beverageTimes = ["GreenTea": 120, "Milk": 180, "Coffee": 420]
+    @IBOutlet weak var titleLabel: UILabel!
     
-    var secondsRemaining = 60
-    
+    let beverageTimes = ["Green Tea": 25, "Milk": 10, "Coffee": 30]
     var timer = Timer()
+    var totalTime = 0
+    var secondsPassed = 0
+    
+    var drinks: String = ""
+    var sound: AVAudioPlayer?
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        do {
+            let url = Bundle.main.url(forResource: "chime_sound", withExtension: "mp3")!
+            sound = try AVAudioPlayer(contentsOf: url)
+            sound?.prepareToPlay()
+        } catch {
+            print("Error loading sound file: \(error)")
+        }
     }
     
-    
+    @IBOutlet weak var progressBar: UIProgressView!
     @IBAction func beverageSelected(_ sender: UIButton) {
         
         timer.invalidate()
-        
         let drinks = sender.currentTitle!
+        totalTime = beverageTimes[drinks]!
         
-        secondsRemaining = beverageTimes[drinks]!
+        progressBar.progress = 0.0
+        secondsPassed = 0
+        titleLabel.text = "Preparing your \(drinks).."
         
         timer = Timer.scheduledTimer(timeInterval: 1.0, target: self, selector: #selector(updateTimer), userInfo: nil, repeats: true)
     }
+    
     @objc func updateTimer() {
-        if secondsRemaining > 0 {
-            print("\(secondsRemaining)seconds until done")
-            secondsRemaining -= 1
+        if secondsPassed < totalTime {
+            
+            secondsPassed += 1
+            progressBar.progress = Float(secondsPassed) / Float(totalTime)
+            
+        } else {
+            timer.invalidate()
+            titleLabel.text = "Here's your Drink!"
+            sound?.play()
         }
     }
     
